0418bc273c570647fb738aed7db22147f1e91473
Omer Golan <gomer@post.bgu.ac.il>
2021-12-29 05:56:10 -0500

    step one in progress

--- src/main/java/StepOne/StepOne.java
+++ src/main/java/StepOne/StepOne.java
@@ -3,80 +3,98 @@ package StepOne;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.mapreduce.lib.output.MapFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+
 
 public class StepOne {
 
-    public static class MapperClass extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private final Text word = new Text();
-        private static final Log log = LogFactory.getLog(StepOne.class);
+    public static class MapperClass
+            extends Mapper<Object, Text, Text, MapWritable>{
 
-        @Override
-        public void map(LongWritable key, Text value, Context context) throws IOException,  InterruptedException{
-            System.out.println(value.toString());
-            log.info(value.toString());
-            context.write(value,one);
-
-//            String[] lines=value.toString().split("\t");
-//            for(String line: lines){
-//                System.out.println(line);
-//            }
-//            StringTokenizer itr = new StringTokenizer(value.toString());
-//            while (itr.hasMoreTokens()) {
-//                word.set(itr.nextToken());
-//                context.write(word, one);
-//            }
-        }
-    }
-
-    public static class ReducerClass extends Reducer<Text,IntWritable,Text,IntWritable> {
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,  InterruptedException {
-            int sum = 0;
-            for (IntWritable value : values) {
-                sum += value.get();
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String[] line = value.toString().split("\t");
+            if (line.length==0){
+                return;
             }
-            context.write(key, new IntWritable(sum));
+            String[] threeGram = line[0].split(" ");
+            int occurrences = Integer.parseInt(line[2]);
+
+            //<w1w2w3,data>
+            String threeGramString=threeGram[0]+" "+threeGram[1]+" "+threeGram[2];
+            MapWritable map1=new MapWritable();
+
+            map1.put(new Text("w1w2w3"),new IntWritable(occurrences));
+
+            context.write(new Text(threeGramString),map1);
+
+            //<w1w2*,data>
+            String twoGramString=threeGram[0]+" "+threeGram[1]+" "+"*";
+
+            MapWritable map2=new MapWritable();
+
+            map2.put(new Text("occurrences"),new IntWritable(occurrences));
+
+            context.write(new Text(twoGramString),map2);
         }
     }
 
-    public static class PartitionerClass extends Partitioner<Text, IntWritable> {
-        @Override
-        public int getPartition(Text key, IntWritable value, int numPartitions) {
-            return key.hashCode() % numPartitions;
+    public static class ReducerMap
+            extends Reducer<Text,MapWritable,Text,MapWritable> {
+        private IntWritable w1w2Sum = new IntWritable(0);
+
+        public void reduce(Text key, Iterable<MapWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            String[] threeGram = key.toString().split(" ");
+            int sum = 0;
+            if(threeGram[2].equals("*")){
+                for(MapWritable map : values){
+                    IntWritable occurrences = (IntWritable) map.get(new Text("occurrences"));
+                    sum += occurrences.get();
+                }
+                w1w2Sum.set(sum);
+            }
+            else{
+                for(MapWritable map : values){
+                    IntWritable occurrences = (IntWritable) map.get(new Text("w1w2w3"));
+                    sum += occurrences.get();
+                }
+
+                MapWritable map=new MapWritable();
+
+                map.put(new Text("w1w2w3"),new IntWritable(sum));
+                map.put(new Text("w1w2"),new IntWritable(w1w2Sum.get()));
+
+                context.write(key,map);
+
+            }
+
         }
     }
 
-
-    public static void main (String[] args) throws Exception {
-        Configuration jobConf = new Configuration();
-        Job job = Job.getInstance(jobConf);
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "step one");
         job.setJarByClass(StepOne.class);
         job.setMapperClass(MapperClass.class);
-        job.setPartitionerClass(PartitionerClass.class);
-        job.setCombinerClass(ReducerClass.class);
-        job.setReducerClass(ReducerClass.class);
+        job.setCombinerClass(StepOne.ReducerMap.class);
+        job.setReducerClass(StepOne.ReducerMap.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[1]));
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setInputFormatClass(SequenceFileInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setOutputValueClass(MapWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
