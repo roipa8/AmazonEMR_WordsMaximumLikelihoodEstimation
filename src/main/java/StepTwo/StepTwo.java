3cb52c686cd5132f1a8bd8c935316fc9125bef5b
vagrant <roipa@post.bgu.ac.il>
2021-12-29 09:00:53 -0500

    step 2 complete

--- src/main/java/StepTwo/StepTwo.java
+++ src/main/java/StepTwo/StepTwo.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 
 public class StepTwo {
     protected static int w2w3Sum = 0;
+    protected static int w2Sum = 0;
     public static class MapperClass
             extends Mapper<Text, MapWritable, Text, MapWritable>{
 
@@ -26,18 +27,27 @@ public class StepTwo {
             String[] threeGram = key.toString().split(" ");
 
             //SHIFT LEFT WITH ENGLISH!! TODO SHIFT RIGHT WITH HEB
-            String threeGramString=threeGram[1]+" "+threeGram[2]+" "+threeGram[0];
+            String modifiedThreeGram=threeGram[1]+" "+threeGram[2]+" "+threeGram[0];
             //<w1w2w3,data>
-            context.write(new Text(threeGramString),value);
+            context.write(new Text(modifiedThreeGram),value);
 
             //<*w2w3,data>//SHIFT LEFT WITH ENGLISH!! TODO SHIFT RIGHT WITH HEB
             String twoGramString=threeGram[0]+" "+threeGram[1]+" *";
 
-            MapWritable map2=new MapWritable();
+            MapWritable map1=new MapWritable();
             IntWritable occurrences = (IntWritable) value.get(new Text("w1w2w3"));
+            map1.put(new Text("occurrences"),occurrences);
+
+            context.write(new Text(twoGramString),map1);
+
+            //<*w2*,data>//SHIFT LEFT WITH ENGLISH!! TODO SHIFT RIGHT WITH HEB
+            String oneGramString=threeGram[0]+" *"+" *";
+
+            MapWritable map2=new MapWritable();
+            occurrences = (IntWritable) value.get(new Text("w1w2w3"));
             map2.put(new Text("occurrences"),occurrences);
 
-            context.write(new Text(twoGramString),map2);
+            context.write(new Text(oneGramString),map2);
         }
     }
 
@@ -46,6 +56,7 @@ public class StepTwo {
         public  void setup(Context context) throws IOException, InterruptedException {
             super.setup(context);
             w2w3Sum=context.getConfiguration().getInt("w2w3Sum",0);
+            w2Sum=context.getConfiguration().getInt("w2Sum",0);
         }
 
         public void reduce(Text key, Iterable<MapWritable> values,
@@ -54,18 +65,28 @@ public class StepTwo {
             String[] threeGram = key.toString().split(" ");
             int sum = 0;
             if(threeGram[2].equals("*")){
-                for(MapWritable map : values){
-                    IntWritable occurrences = (IntWritable) map.get(new Text("occurrences"));
-                    sum += occurrences.get();
+                if(threeGram[1].equals("*")){
+                    for(MapWritable map : values){
+                        IntWritable occurrences = (IntWritable) map.get(new Text("occurrences"));
+                        sum += occurrences.get();
+                    }
+                    w2Sum = sum;
                 }
-                w2w3Sum = sum;
+                else{
+                    for(MapWritable map : values){
+                        IntWritable occurrences = (IntWritable) map.get(new Text("occurrences"));
+                        sum += occurrences.get();
+                    }
+                    w2w3Sum = sum;
+                }
             }
             else{
                 for(MapWritable map : values){//I HAVE ONLY ONE KEY OF 3GRAM
                     map.put(new Text("w2w3"),new IntWritable(w2w3Sum));
+                    map.put(new Text("w2"),new IntWritable(w2Sum));
                     //SHIFT RIGHT WITH ENGLISH!! TODO SHIFT LEFT WITH HEB
-                    String threeGramString=threeGram[2]+" "+threeGram[0]+" "+threeGram[1];
-                    context.write(new Text(threeGramString),map);
+                    String originalThreeGram=threeGram[2]+" "+threeGram[0]+" "+threeGram[1];
+                    context.write(new Text(originalThreeGram),map);
                 }
             }
         }
